b8c0b269b18b90c85cfde556b8a88f4bbc3f4b9e
Luciano Amoruso <luciano01amoruso@gmail.com>
2022-07-07 10:12:30 -0300

    Primer Commit

---
+++ playlistsaviorExecution.ps1
@@ -0,0 +1,6 @@
+cd C:\bashi\projects\youtubePlaylistSavior
+.\youtubesavior-env\Scripts\activate
+python '.\smash playlist.py'
+python '.\music playlist.py'
+deactivate
+exit
---
+++ smash playlist.py
@@ -0,0 +1,52 @@
+from googleapiclient.discovery import build
+from datetime import date
+import json
+
+# smash = open('smash.txt', 'rb')
+
+api_key = 'AIzaSyDsYE8KPNDbcVcC5Sfog1uTS8VvZj3RCNY'
+
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+titulos = []
+nextPageToken = None
+
+while True:
+    pl_request = youtube.playlistItems().list(
+        part='contentDetails',
+        playlistId='PL_hXMzbIDwY6JUkTmCCoL1BwVIDHQpnRb',
+        maxResults=50,
+        pageToken=nextPageToken
+    )
+
+    pl_response = pl_request.execute()
+    vid_ids = [item['contentDetails']['videoId'] for item in pl_response['items']]
+
+    vid_request = youtube.videos().list(
+        part="snippet",
+        id=','.join(vid_ids)
+    )
+
+    vid_response = vid_request.execute()
+    for item in vid_response['items']:
+        titulos.append(item['snippet']['title'])
+
+    nextPageToken = pl_response.get('nextPageToken')
+
+    if not nextPageToken:
+        break
+
+numeracion = []
+num = 1
+while num < len(titulos) + 1:
+    numeracion.append(f'{num}) ')
+    num += 1
+
+# newlines = ['\n' for x in numeracion]
+parejas = tuple(zip(numeracion, titulos))
+parejas = list(map(lambda x: ''.join(x), parejas))
+
+with open(f'C:/bashi/playlists/generated/smash {date.today()}.txt', 'xb') as fp:
+    fp.write('\n'.join(parejas).encode('UTF-8'))
+
+print('<<smash file created successfully>>')
