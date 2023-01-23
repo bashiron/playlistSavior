e6c5166cac0beed4e8e93b74289b25cd052ebd58
Bashiron <bioshiron@gmail.com>
2023-01-23 08:14:11 -0300

    created mock for Savior

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,9 +1,10 @@
 import os.path
 
 from definitions import PROJECT_ROOT
-from cli import cli
+import cli
+from cli import cli as cli_app
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from click.testing import CliRunner
 
 class TestCLI(TestCase):
@@ -19,27 +20,32 @@ class TestCLI(TestCase):
         runner = CliRunner()
         inp_1 = 'blablabla'
         inp_2 = 'N'
-        res = runner.invoke(cli, ['setup'], input='\n'.join([inp_1, inp_2]))
+        res = runner.invoke(cli_app, ['setup'], input='\n'.join([inp_1, inp_2]))
         env = open(os.path.join(PROJECT_ROOT, '.env'), 'rt', encoding='utf-8')
         assert env.readline() == 'DEV_KEY=%s' % inp_1
         env.close()
         assert res.exit_code == 0
         assert 'setup complete' in res.output
 
-    def test_add_playlist(self):
+    @patch("cli.Savior")
+    def test_add_playlist(self, MockSavior):
+        m_savior = MockSavior()
+        cli.savior = m_savior
+        # TODO change method behavior
+        m_savior.add_playlist = MagicMock(...)
         runner = CliRunner()
-        res = runner.invoke(cli, ['add-playlist', 'SMASH', 'https://youtube.com/playlist?list=PL_hXMzbIDwY6JUkTmCCoL1BwVIDHQpnRb'])
+        res = runner.invoke(cli_app, ['add-playlist', 'SMASH', 'https://youtube.com/playlist?list=PL_hXMzbIDwY6JUkTmCCoL1BwVIDHQpnRb'])
         assert res.exit_code == 0
         assert 'playlist id is PL_hXMzbIDwY6JUkTmCCoL1BwVIDHQpnRb' in res.output
 
     def test_save_all(self):
         runner = CliRunner()
-        res = runner.invoke(cli, ['save'])
+        res = runner.invoke(cli_app, ['save'])
         assert res.exit_code == 0
         assert 'saving all playlists' in res.output
 
     def test_save_some(self):
         runner = CliRunner()
-        res = runner.invoke(cli, ['save', '-p', 'SMASH', 'music'])
+        res = runner.invoke(cli_app, ['save', '-p', 'SMASH', 'music'])
         assert res.exit_code == 0
         assert 'saving specified playlists' in res.output
